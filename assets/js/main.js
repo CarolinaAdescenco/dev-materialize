aed034593b9971cfc402d6929b296780a04a20a4
carolinaadescenco <carolinaadescencofernandes@hotmail.com>
2019-08-25 22:35:44 -0300

    Projeto utilizando Framework Materialize

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    $('select').formSelect();
+
+    
+    // $('.carousel.carousel-slider').carousel({
+    //     duration: 200,
+    //     fullWidth: true,
+    //     indicators: true
+    // });
+});
